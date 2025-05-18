70ffa9c72d7099af177baa9d63c6bc361fb60ccf
devmunro <defmunro@gmail.com>
2025-05-18 15:46:36 +0100

    fix ESM issue

--- bin/otter-store.js
+++ bin/otter-store.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 import { copyFileSync } from "fs";
-import { join } from "path";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const templatePath = join(__dirname, "../templates/otter-store-template.js");
 const targetPath = join(process.cwd(), "otter-store.js");
